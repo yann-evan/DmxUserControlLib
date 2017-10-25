76c13950952955def55c286f14cc94e4f648593f
yann-evan <yann.evan.morcet@gmail.com>
2017-10-25 23:11:12 +0200

    Gestion Flashing Mode sur la config des Leds RGB

--- DmxUserControlLib/LaunchpadMap.xaml.cs
+++ DmxUserControlLib/LaunchpadMap.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DmxUserControlLib
 {
@@ -20,9 +11,68 @@ namespace DmxUserControlLib
     /// </summary>
     public partial class LaunchpadMap : UserControl
     {
+        public event EventHandler BT_Click;
+        public List<int> selectedBT = new List<int>();
+        public List<int> selectedSystemBT = new List<int>();
+
         public LaunchpadMap()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button bt = sender as System.Windows.Controls.Button;
+            int BT_ID;
+            bool Is_BT_system;
+
+            if (Grid.GetRow(bt) == 1)
+            {
+                BT_ID = Grid.GetColumn(bt);
+                Is_BT_system = true;
+                if(is_BT_selected(selectedSystemBT, BT_ID))
+                {
+                    bt.SetValue(BackgroundProperty, Brushes.Gray);
+                    selectedSystemBT.Remove(BT_ID);
+                }
+                else
+                {
+                    bt.SetValue(BackgroundProperty, Brushes.LightBlue);
+                    selectedSystemBT.Add(BT_ID);
+                }
+            }
+            else
+            {
+                BT_ID = Grid.GetColumn(bt) + ((Grid.GetRow(bt) - 2) * 16);
+                Is_BT_system = false;
+                if (is_BT_selected(selectedBT, BT_ID))
+                {
+                    bt.SetValue(BackgroundProperty, Brushes.White);
+                    selectedBT.Remove(BT_ID);
+                }
+                else
+                {
+                    bt.SetValue(BackgroundProperty, Brushes.LightBlue);
+                    selectedBT.Add(BT_ID);
+                }
+            }
+
+            if (this.BT_Click != null)
+            {
+                this.BT_Click(this, e);
+            }
+        }
+
+        private Boolean is_BT_selected(List<int> sel, int BT_ID)
+        {
+            foreach(int ID in sel)
+            {
+                if(ID == BT_ID)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
